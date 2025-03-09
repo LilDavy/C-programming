15d8ea4af1deadd3a484ddd942293d38f17c190b
LilDavy <lildavy58@gmail.com>
2025-03-09 18:20:07 +0300

    1 dimension matrix

---
+++ 1 Dimension matrix.cpp
@@ -0,0 +1,22 @@
+
+/*
+Author:David Otieno
+Reg No.BSCIT-05-0014/2024
+date 3/3/2025
+version 1
+*/
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    // One-dimensional matrix
+    int matrix1D[5] = {1, 2, 3, 4, 5};
+ 
+    for (int i = 0; i < 5; ++i) {
+        cout << matrix1D[i] << " ";
+    }
+    cout << std::endl;
+
+    return 0;
+}
